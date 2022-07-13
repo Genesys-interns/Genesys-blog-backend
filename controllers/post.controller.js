6fa3dffdf03ed4348ac900109248db60d8d4af5b
Emmanuel <emmanuelugwueze6@gmail.com>
2022-07-13 13:56:19 +0100

    created post controllers,fixes,installed packages

---
+++ controllers/post.controller.js
@@ -0,0 +1,16 @@
+import postService from '../services/post.service.js';
+
+class PostController {
+  async createPost(req, res) {
+    const body = {
+      title: req.body.title,
+      description: req.body.description,
+      category: req.body.category,
+      userId: req.body.userId,
+      body: req.body.body,
+      image: req.file.originalname
+    };
+    const post = await postService.createPost(body);
+    return res.status(201).send({ status: true, message: 'post created successfully', body: post });
+  }
+}
--- models/post.model.js
+++ models/post.model.js
@@ -8,7 +8,7 @@ const postSchema = mongoose.Schema({
   },
   description: {
     required: true,
-    tyoe: String
+    type: String
 
   },
   category: {
