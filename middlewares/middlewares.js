8f6d9ddccc73b8c4e2557c8e5178300d5b5ec0f1
Emmanuel <emmanuelugwueze6@gmail.com>
2022-07-13 11:22:56 +0100

    changes

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -4,13 +4,15 @@ import cors from 'cors';
 import database from '../config/db.config.js';
 
 const middleware = (app) => {
- 
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-    app.use(morgan());
-    app.use(express.static('uploads'));
+  app.use(morgan());
+  app.use(express.static('uploads'));
   app.use(cors());
   database();
+  app.use('*', (req, res) => {
+    res.status(200).send('Server is up and running,check the API documentation');
+  });
 };
 
 export default middleware;
