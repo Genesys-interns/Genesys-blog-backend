24ea1b80f27ca7d21c14284de8bbfbd75e3cde7d
Emmanuel <emmanuelugwueze6@gmail.com>
2022-07-15 08:01:55 +0100

    user model

---
+++ services/user.service.js
@@ -0,0 +1,17 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable import/extensions */
+import { User } from '../models/user.model.js';
+
+class UserService {
+  async create(data) {
+    const newUser = await User.create(data);
+    return newUser;
+  }
+
+  async findByEmail(data) {
+    const user = await User.findOne({ email: data.email });
+    return user;
+    }
+}
+
+export default new UserService();
