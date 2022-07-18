6f110d29b677a50c88ce0a064b25d850b08c2da5
Rene0115 <enere0115@gmail.com>
2022-07-18 13:05:28 +0100

    space

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,23 +1,21 @@
+/* eslint-disable import/extensions */
 import express from 'express';
 import multer from 'multer';
 import postController from '../controllers/post.controller.js';
 
-
-
 const postRouter = express.Router();
 
 const storage = multer.diskStorage({
-  destination:function (req, file, cb) {
+  destination(req, file, cb) {
     cb(null, 'uploads/');
   },
-  filename:function(req, file, cb) {
+  filename(req, file, cb) {
     cb(null, file.originalname);
   }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage });
 postRouter.post('/', upload.single('image'), postController.createPost);
 postRouter.get('/', postController.getPosts);
-
 
 export default postRouter;
