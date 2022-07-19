86417e8fb3e1ae3f356c2d4df4cd6b01f8d8c84e
Emmanuel <emmanuelugwueze6@gmail.com>
2022-07-19 23:05:31 +0100

    fetch comments

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -4,11 +4,19 @@ import commentService from '../services/comment.service.js';
 /* eslint-disable class-methods-use-this */
 class CommentController {
   async postComments(req, res) {
-      const comment = await commentService.postComment(req.body);
+    const comment = await commentService.postComment(req.body);
     if (_.isEmpty(comment)) {
       return res.status(404).send({ status: false, message: 'unable to post comment' });
-      }
-      return res.status(200).send({ status: true, message: 'comment posted successfully' });
+    }
+    return res.status(200).send({ status: true, message: 'comment posted successfully' });
+  }
+
+  async getComments(req, res) {
+    const comment = await commentService.getComment(req.params.id);
+    if (_.isEmpty(comment)) {
+      return res.status(404).send({ status: false, message: 'no comments available' });
+    }
+    return res.status(200).send({ status: true, body: comment });
   }
 }
 
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -9,6 +9,7 @@ import validator from '../validators/validator.js';
 const postRouter = express.Router();
 
 postRouter.post('/comments', checkAuth, validator(commentvalidator), commentController.postComments);
+postRouter.get('/comments/:id', commentController.getComments);
 
 
 export default postRouter;
