18eabce3d7195217e303a8f13f963cd6dc571260
Christian <onovochristian19@gmail.com>
2022-07-21 14:26:03 +0100

    =initial commit 2

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,11 +1,6 @@
 /* eslint-disable import/extensions */
 import express from 'express';
-import noteRouter from './post.routes.js';
-import userGoogleRouter from './userGoogle.routes.js';
 
 const router = express.Router();
-
-router.use('/post', noteRouter);
-router.use('/users/google_signin', userGoogleRouter);
 
 export default router;
