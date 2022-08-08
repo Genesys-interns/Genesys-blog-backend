dc6ece330f3340f220210495c3a063ed652902f1
Emmanuel <emmanuelugwueze6@gmail.com>
2022-08-08 16:17:15 +0100

    better user response message

--- controllers/user.controller.v2.js
+++ controllers/user.controller.v2.js
@@ -16,7 +16,7 @@ import commentController from './comment.controller.v2.js';
 
 class UserControllerV2 {
   async create(req, res) {
-    const user = UserService.findByEmail(req.body);
+    const user = await UserService.findByEmail(req.body);
     if (!_.isEmpty(user)) {
       return res.status(400).send({
         success: false,
